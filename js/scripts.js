08ea525d2c7136ac721fe77a2d3ae0bafec302b8
Benitha250 <benithauwase05@gmail.com>
2020-10-04 12:32:42 -0400

    add jquery codes to add effect to the portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,46 @@ $(document).ready(function(){
     });
     
 });
+$(document).ready(function(){
+ 
+    $("#card1").hover(function(){
+        $(".card1-words").css("opacity", 0.6);
+        $(".card1-words").fadeIn(800);
+        $(".card1-words").fadeOut(100).stop();
+    });
+    $("#card2").hover(function(){
+        $(".card2-words").css("opacity", 0.6);
+        $(".card2-words").fadeIn(800);
+        $(".card2-words").fadeOut(100).stop();
+    });
+    $("#card3").hover(function(){
+        $(".card3-words").css("opacity", 0.6);
+        $(".card3-words").fadeIn(800);
+        $(".card3-words").fadeOut(100).stop();
+    });
+    $("#card4").hover(function(){
+        $(".card4-words").css("opacity", 0.6);
+        $(".card4-words").fadeIn(800);
+        $(".card4-words").fadeOut(100).stop();
+    });
+    $("#card5").hover(function(){
+        $(".card5-words").css("opacity", 0.6);
+        $(".card5-words").fadeIn(800);
+        $(".card5-words").fadeOut(100).stop();
+    });
+    $("#card6").hover(function(){
+        $(".card6-words").css("opacity", 0.6);
+        $(".card6-words").fadeIn(800);
+        $(".card6-words").fadeOut(100).stop();
+    });
+    $("#card7").hover(function(){
+        $(".card7-words").css("opacity", 0.6);
+        $(".card7-words").fadeIn(800);
+        $(".card7-words").fadeOut(100).stop();
+    });
+    $("#card8").hover(function(){
+        $(".card8-words").css("opacity", 0.6);
+        $(".card8-words").fadeIn(800);
+        $(".card8-words").fadeOut(100).stop();
+    });
+});
